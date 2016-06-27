f834174a1c13009d289e1fdab9faef57a3ea5aef
bobbimon <tobbe_lobbe88@hotmail.com>
2016-06-28 01:29:53 +0200

    Playing around

---
+++ verbose-waffle-core/verbose-waffle-core-user/verbose-waffle-core-user-domain/src/main/java/com/verbose/waffle/core/user/domain/user.java
@@ -0,0 +1,10 @@
+package com.verbose.waffle.core.user.domain;
+
+import java.io.Serializable;
+
+/**
+ * Created by TobNil on 2016-06-28.
+ */
+public class user implements Serializable {
+    private static final long serialVersionUID = 1L;
+}
